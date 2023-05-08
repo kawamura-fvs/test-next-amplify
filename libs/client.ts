fee9e52a1ddd61661af48b8ed8aa8add0f454f74
kawamura-fvs <97089001+kawamura-fvs@users.noreply.github.com>
2023-05-08 17:40:14 +0900

    not microcms-sdk

--- libs/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN ?? 'no service domain',
-  apiKey: process.env.API_KEY ?? 'no api key',
-});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,7 @@
-import { client } from "libs/client";
-import type { News } from "types/news";
-
-type Props = {
-  news: News;
-};
-
-export default function Home({ news }: Props) {
+export default function Home() {
   return (
     <div>
-      <h1>{news.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: news.test.test }} />
+      aaa
     </div>
   )
 }
-
-
-export const getStaticProps = async () => {
-  const news = await client.get({ endpoint: "news", contentId: "671211508175" });
-  return {
-    props: {
-      news: news,
-    },
-  };
-};
